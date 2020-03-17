e0332eb150f0bd4998c52c78edad8af2516743c9
ZongSheng <fancyian05180910@gmail.com>
2020-03-17 14:10:39 +0800

    網頁微調

--- js/main.js
+++ js/main.js
@@ -16,4 +16,23 @@ $(document).ready(function(){
 		$(window).stellar();
 	}
 
+	// from
+	var $form = $('form#contact-form'),
+		url = 'https://script.google.com/macros/s/AKfycbw66o6P59WAZJC7S5LzY48WTUZ-ihvEuUoTpphvn1QZngLynqKs/exec'
+
+	$('#submit-form').on('click', function (e) {
+		e.preventDefault();
+		var jqxhr = $.ajax({
+			url: url,
+			method: "GET",
+			dataType: "json",
+			data: $form.serialize()
+		}).success(
+			$('#userName').val(""),
+			$('#userTel').val(""),
+			$('#userEmail').val(""),
+			$('#userText').val("")
+		);
+	})
+
 })
